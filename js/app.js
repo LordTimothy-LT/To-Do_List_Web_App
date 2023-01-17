68bb7ad3cc555e788a7f898c8a2d6ce6b6162abb
Samuel Timothy <lordtimothy11@gmail.com>
2023-01-17 09:57:03 +0100

    small change

--- js/app.js
+++ js/app.js
@@ -1,62 +1,59 @@
-document.getElementById('form-Task').addEventListener('submit', saveTask);
- 
+document.getElementById("form-Task").addEventListener("submit", saveTask);
+
 // Save new To-Do
 function saveTask(e) {
- 
-  let title = document.getElementById('title').value;
-  let description = document.getElementById('description').value;
- 
- 
+  let title = document.getElementById("title").value;
+  let description = document.getElementById("description").value;
+  let date = document.getElementById("date").value;
+
   let task = {
     title,
-    description
+    description,
+    date,
   };
- 
-  if (localStorage.getItem('tasks') === null) {
+
+  if (localStorage.getItem("tasks") === null) {
     let tasks = [];
     tasks.push(task);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem("tasks", JSON.stringify(tasks));
   } else {
-    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    let tasks = JSON.parse(localStorage.getItem("tasks"));
     tasks.push(task);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem("tasks", JSON.stringify(tasks));
   }
- 
+
   getTasks();
- 
+
   // Reset form-Task
-  document.getElementById('form-Task').reset();
+  document.getElementById("form-Task").reset();
   e.preventDefault();
- 
 }
- 
-// Delete To-Do 
+
+// Delete To-Do
 function deleteTask(title) {
- 
-  let tasks = JSON.parse(localStorage.getItem('tasks'));
+  let tasks = JSON.parse(localStorage.getItem("tasks"));
   for (let i = 0; i < tasks.length; i++) {
     if (tasks[i].title == title) {
       tasks.splice(i, 1);
     }
   }
- 
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
   getTasks();
 }
- 
+
 // Show To-Do List
 function getTasks() {
- 
-  let tasks = JSON.parse(localStorage.getItem('tasks'));
-  let tasksView = document.getElementById('tasks');
-  tasksView.innerHTML = '';
- 
+  let tasks = JSON.parse(localStorage.getItem("tasks"));
+  let tasksView = document.getElementById("tasks");
+  tasksView.innerHTML = "";
+
   for (let i = 0; i < tasks.length; i++) {
     let title = tasks[i].title;
     let description = tasks[i].description;
- 
-    tasksView.innerHTML +=
-      `<div class="card mb-3">
+    let Date = tasks[i].date;
+
+    tasksView.innerHTML += `<div class="card mb-3">
         <div class="card-body">
         <div class="row">
           <div class="col-sm-3 text-left">
@@ -65,6 +62,9 @@ function getTasks() {
           <div class="col-sm-7 text-left">
             <p>${description}</p>
           </div>
+          <div class="col-sm-3 text-right">
+            <p>${Date}</p>
+          </div>
           <div class="col-sm-2 text-right">
             <a href="#" onclick="deleteTask('${title}')" class="btn btn-danger ml-5">X</a>
           </div>
@@ -72,7 +72,6 @@ function getTasks() {
        </div>
       </div>`;
   }
- 
 }
- 
+
 getTasks();
